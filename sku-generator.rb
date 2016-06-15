5e84fe6c0617b4e56c6961f07f2290c09f455ecc
Dustin McNabb <dustin.mcnabb@gmail.com>
2016-06-14 23:21:56 -0700

    Refactored the read csv code to be more readable.

--- sku-generator.rb
+++ sku-generator.rb
@@ -7,15 +7,12 @@ updated_inventory = []
 
 # Generate SKUs using an all caps abbreviation of the Category, Name, Variant Name, Variant Number, and a random number.
 def gen_skus(product,num)
+    skus = []
     product_category = product["Category"].to_s.upcase.tr('^0-9A-Z ', '').split(' ').map { |word| word.slice(0..3) }.join.slice(0..6)
     product_name = product["Name"].to_s.upcase.tr('^0-9A-Z ', '').split(' ').map { |word| word.slice(0..3) }.join.slice(0..6)
     product_variant = product["Variant #{num} - Name"].to_s.upcase.tr('^0-9A-Z', '').slice(0..2)
     product["Variant #{num} - SKU"] = [product_category, product_name, product_variant, (num.to_s + rand(100..1000).to_s)].join("-")
-
-end
-
-def update_inventory(product,updated_inventory)
-    updated_inventory << product
+    skus << product["Variant #{num} - SKU"]
 end
     
 def create_updated_csv(column_names,updated_inventory)
@@ -27,19 +24,16 @@ def create_updated_csv(column_names,updated_inventory)
     end
 end
 
-CSV.foreach('inventory.csv', headers: true) do |product|
+CSV.foreach('itemsbulk-template.csv', headers: true) do |product|
 # Currently Square allows 40 variants per item. 
 # Each one needs it's own SKU. We have to iterate to 41 to prevent ignoring items with 40 variants.
-variant = (1..41)
+num = 1
 column_names = product.headers
-    variant.each do |num|
-        if product["Variant #{num} - Name"] && product["Variant #{num} - Name"].length != 0
-            gen_skus(product,num)
-        else 
-            update_inventory(product,updated_inventory)
-            break
-        end
-    end
+    while product["Variant #{num} - Name"] && product["Variant #{num} - Name"].length != 0
+        gen_skus(product,num)
+        num += 1
+    end 
+updated_inventory << product
 end
 
 create_updated_csv(column_names,updated_inventory)
